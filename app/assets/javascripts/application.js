ae00ba0986fd2b7494ff43330a4092f1125690a3
Cory Williams <cjwillia@andrew.cmu.edu>
2014-09-23 12:36:37 -0400

    changes to bg_check show page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -57,9 +57,11 @@ $(document).ready(function(){
   });
 
   //code to fade out alert boxes
+  /*
   $('.alert-box').fadeIn('normal', function() {
       $(this).delay(2000).fadeOut();
    });
+  */
 
   //datepicker code
   $('.datepicker').fdatepicker({
