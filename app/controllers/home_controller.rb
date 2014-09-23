227a54c5e7b31a56d82c42a6ad4cded81dfe89d1
Sethu Prakasam <psethu27@gmail.com>
2014-09-23 14:05:20 -0400

    Have 2 columns working, but not able to get them side by side

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
     def index
+    	@bgChecks_req = BgCheck.all.requested
+    	@bgChecks_pCriminal = BgCheck.all.passed_criminal
         render 'home/index'
     end
 end
