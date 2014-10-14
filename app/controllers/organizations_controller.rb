7b6c42d48e0e047f50a8d1ec3c394223f7e9da66
Sethu Prakasam <psethu27@gmail.com>
2014-10-14 01:42:28 -0400

    additional comments for clarification

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -112,7 +112,7 @@ class OrganizationsController < ApplicationController
            if @membership.save
               redirect_to organization_path(org_id), notice: "Added member: #{@indiv.f_name}"
            else
-              redirect_to organization_path(org_id),  notice: "Cannot send notice."
+              redirect_to edit_organization_path(org_id),  notice: "Member in system. Could not add member."
             end
      
       else
@@ -131,8 +131,8 @@ class OrganizationsController < ApplicationController
             if @orgMailer.deliver
               redirect_to organization_path(org_id), notice: "Notice sent to \"#{@orgMailer.currently_registered_email}\""
             else
-              redirect_to organization_path(org_id)
-              flash.now[:error] = 'Cannot send notice.'
+              redirect_to edit_organization_path(org_id)
+              flash.now[:error] = 'Member not in system. Could not send notice.'
             end
       end 
   end  
