8d970b95ae2636ab7e12675541505a144137bf1d
Sethu Prakasam <psethu27@gmail.com>
2014-10-28 17:33:28 -0400

    fixed home url for regular user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,19 @@
 class HomeController < ApplicationController
     def index
-        if current_user.admin?
-            redirect_to bg_checks_path
+        # if user exists route to appropriate bg_check path
+        if  current_user
+            # use this to redirect to a new or existing bg_check for a user
+            bg_c = current_user.individual.bg_check
+            if current_user.admin?
+                redirect_to bg_checks_path
+            elsif (bg_c.nil?)
+                redirect_to new_bg_check_path
+            else
+                redirect_to bg_check_path(bg_c)
+            end
         else
             render 'home/index'
-        end
+        end            
     end
 
     def signed_up_confirm
